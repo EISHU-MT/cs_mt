7ce07f27be26466f45486f470dbb5e73712262e4
EISHU92 <donatto555@gmail.com>
2023-03-30 20:03:55 -0400

    Add warnings to defuser in field [on_use]

--- mods/cs_core/core/defuser.lua
+++ mods/cs_core/core/defuser.lua
@@ -5,45 +5,56 @@ minetest.register_craftitem("core:defuser", {
 	on_use = function(item, player)
 		local inf = clua.get_player_inf(player)
 		local pos = inf.pos --link
-			if c4.get_status() then -- if the bomb is planted or not!
-				statspawn = c4.pos
-				if pos.x < statspawn.x + 2 and pos.x > statspawn.x - 2 and pos.y < statspawn.y + 2 and pos.y > statspawn.y - 2 and pos.z < statspawn.z + 2 and pos.z > statspawn.z - 2 then
-					function defuser_interrupted()
-						return false
-					end
-					clua.global("defuser_guy", player:get_player_name())
-					his_hud = player:hud_add({
-							hud_elem_type = "text",
-							name = "defuser_timer",
-							scale = {x = 1.5, y = 1.5},
-							position = {x = 0.5, y = 0.5},
-							offset = {x = 0, y = 20},
-							--size = {x = 2},
-							alignment = {x = "center", y = "down"},
-							--alignment = {x = 0, y = -1},
-							text = "Defusing! this will took 5 secs",
-							number = 0xCECECE,
-					})
-					core.after(5, function(user)
-						if (defuser_interrupted() == false) then
-							annouce.winner("counter", "Congrats to "..user:get_player_name().." for defusing the bomb!")
-							cs_match.finished_match(csgo.pot[user:get_player_name()])
-							to_end = nil
+			if ask_for_bomb() then
+				if c4.get_status() then -- if the bomb is planted or not!
+					statspawn = c4.pos
+					if pos.x < statspawn.x + 2 and pos.x > statspawn.x - 2 and pos.y < statspawn.y + 2 and pos.y > statspawn.y - 2 and pos.z < statspawn.z + 2 and pos.z > statspawn.z - 2 then
+						function defuser_interrupted()
+							return false
 						end
-					end, player)
+						clua.global("defuser_guy", player:get_player_name())
+						his_hud = player:hud_add({
+								hud_elem_type = "text",
+								name = "defuser_timer",
+								scale = {x = 1.5, y = 1.5},
+								position = {x = 0.5, y = 0.5},
+								offset = {x = 0, y = 20},
+								--size = {x = 2},
+								alignment = {x = "center", y = "down"},
+								--alignment = {x = 0, y = -1},
+								text = "Defusing! this will took 5 secs",
+								number = 0xCECECE,
+						})
+						core.after(5, function(user)
+							if (defuser_interrupted() == false) then
+								annouce.winner("counter", "Congrats to "..user:get_player_name().." for defusing the bomb!")
+								cs_match.finished_match(csgo.pot[user:get_player_name()])
+								to_end = nil
+								user:hud_remove(his_hud)
+							end
+						end, player)
+					else
+						hud_events.new(player, {
+							text = "(!) Not near at the bomb!",
+							color = "warning",
+							quick = true,
+						})
+					end
 				else
 					hud_events.new(player, {
-						text = "(!) Not near at the bomb!",
+						text = "(!) The bomb inst planted!",
 						color = "warning",
 						quick = true,
 					})
 				end
 			else
 				hud_events.new(player, {
-					text = "(!) The bomb inst planted!",
-					color = "warning",
+					text = "(!!) Its illegal to use defuser on a map that has bomb_planting disabled!\n -CLua",
+					color = "danger",
 					quick = true,
 				})
+				invv = player:get_inventory()
+				invv:remove_item("main", item)
 			end
 		
 	end,
