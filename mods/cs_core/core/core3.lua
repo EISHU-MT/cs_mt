de6d0043523f204800440573ef63f1a81d2ca4fe
EISHU92 <donatto555@gmail.com>
2023-03-31 17:11:18 -0400

    Add queue restart command

--- mods/cs_core/core/core3.lua
+++ mods/cs_core/core/core3.lua
@@ -19,7 +19,28 @@ function minetest.log(typeo, ...)
         old_on_log(typeo, ...)
    -- end, ...)
 end
-
+restart = false
+restartm = "This server is being restarted from operator request"
+-- Restart on finish all matchs
+core.register_chatcommand("queue_restart", {
+	description = "Restart server on finish all matchs",
+	params = "<message",
+	func = function(name, param)
+        --Param is optional
+        restart = true
+        if param then
+            restartm = param
+        else
+            restartm = "This server is being restarted from operator request"
+        end
+    end,
+	privs = {server=true, core=true}
+})
+call.register_on_new_matches(function()
+    if restart then
+        core.shutdown(restartm)
+    end
+end)
 
 
 
