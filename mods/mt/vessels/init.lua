3807a7001de6984acc9899ed06bbac98c611ae35
EISHU92 <donatto555@gmail.com>
2023-04-01 11:16:24 -0400

    Fix crash

--- mods/mt/vessels/init.lua
+++ mods/mt/vessels/init.lua
@@ -7,48 +7,6 @@
 local S = minetest.get_translator("vessels")
 
 
-local vessels_shelf_formspec =
-	"size[8,7;]" ..
-	"list[context;vessels;0,0.3;8,2;]" ..
-	"list[current_player;main;0,2.85;8,1;]" ..
-	"list[current_player;main;0,4.08;8,3;8]" ..
-	"listring[context;vessels]" ..
-	"listring[current_player;main]" ..
-	default.get_hotbar_bg(0, 2.85)
-
-local function update_vessels_shelf(pos)
-	local meta = minetest.get_meta(pos)
-	local inv = meta:get_inventory()
-	local invlist = inv:get_list("vessels")
-
-	local formspec = vessels_shelf_formspec
-	-- Inventory slots overlay
-	local vx, vy = 0, 0.3
-	local n_items = 0
-	for i = 1, 16 do
-		if i == 9 then
-			vx = 0
-			vy = vy + 1
-		end
-		if not invlist or invlist[i]:is_empty() then
-			formspec = formspec ..
-				"image[" .. vx .. "," .. vy .. ";1,1;vessels_shelf_slot.png]"
-		else
-			local stack = invlist[i]
-			if not stack:is_empty() then
-				n_items = n_items + stack:get_count()
-			end
-		end
-		vx = vx + 1
-	end
-	meta:set_string("formspec", formspec)
-	if n_items == 0 then
-		meta:set_string("infotext", S("Empty Vessels Shelf"))
-	else
-		meta:set_string("infotext", S("Vessels Shelf (@1 items)", n_items))
-	end
-end
-
 local vessels_shelf_def = {
 	description = S("Vessels Shelf"),
 	tiles = {"default_wood.png", "default_wood.png", "default_wood.png",
@@ -57,32 +15,8 @@ local vessels_shelf_def = {
 	is_ground_content = false,
 	groups = {choppy = 3, oddly_breakable_by_hand = 2, flammable = 3},
 	sounds = default.node_sound_wood_defaults(),
-
-	on_construct = function(pos)
-		local meta = minetest.get_meta(pos)
-		update_vessels_shelf(pos)
-		local inv = meta:get_inventory()
-		inv:set_size("vessels", 8 * 2)
-	end,
-	can_dig = function(pos,player)
-		local inv = minetest.get_meta(pos):get_inventory()
-		return inv:is_empty("vessels")
-	end,
-	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-		if minetest.get_item_group(stack:get_name(), "vessel") ~= 0 then
-			return stack:get_count()
-		end
-		return 0
-	end,
-	on_blast = function(pos)
-		local drops = {}
-		default.get_inventory_drops(pos, "vessels", drops)
-		drops[#drops + 1] = "vessels:shelf"
-		minetest.remove_node(pos)
-		return drops
-	end,
 }
-default.set_inventory_action_loggers(vessels_shelf_def, "vessels shelf")
+
 minetest.register_node("vessels:shelf", vessels_shelf_def)
 
 minetest.register_craft({
