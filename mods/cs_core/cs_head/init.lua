afeef8a925f21a00a7fd4eed15e23e63eb0e8bb2
EISHU92 <donatto555@gmail.com>
2023-04-01 11:11:23 -0400

    Add new mt mods, add head animation

---
+++ mods/cs_core/cs_head/init.lua
@@ -0,0 +1,29 @@
+function te1a()
+	list = core.get_connected_players()
+	core.after(1.5, te1a)
+end
+te1a()
+local get_connected_players = function() -- solve lag
+	return list
+end
+--minetest.get_connected_players
+local abs = math.abs
+local deg = math.deg
+local basepos = vector.new(0, 6.35, 0)
+local lastdir = {}
+
+minetest.register_globalstep(function(dtime)
+	for _, player in pairs(get_connected_players()) do
+		local pname = player:get_player_name()
+		local ldeg = -deg(player:get_look_vertical())
+
+		if abs((lastdir[pname] or 0) - ldeg) > 4 then
+			lastdir[pname] = ldeg
+			player:set_bone_position("Head", basepos, {x = ldeg, y = 0, z = 0})
+		end
+	end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	lastdir[player:get_player_name()] = nil
+end)
