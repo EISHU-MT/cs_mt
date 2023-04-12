9320716603e3762a202f890cb6d52ad648a79ebf
EISHU92 <donatto555@gmail.com>
2023-04-12 09:53:01 -0400

    Upgrade Map Downloader

--- upgrade_maps.sh
+++ upgrade_maps.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 gh repo clone EISHU92/cs_map
 if [ -d $PWD/cs_map* ]; then
-	if [ -d $PWD/csgo/mods/cs_core/cs_map ]; then
-		mkdir -p $PWD/csgo/mods/cs_core/cs_map/cs_maps
+	if [ -d $PWD/csgo_minetest/mods/cs_core/cs_map ]; then
+		mkdir -p $PWD/csgo_minetest/mods/cs_core/cs_map/cs_maps
 		uPWD=$PWD
 		cd $PWD/cs_map*
 		echo "Moving `ls`"
-		mv * $uPWD/csgo/mods/cs_core/cs_map/cs_maps
+		mv * $uPWD/csgo_minetest/mods/cs_core/cs_map/cs_maps
 	fi
 fi
