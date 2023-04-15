e36febd4785dfd083fedd5d54807c89194aa5a54
EISHU92 <donatto555@gmail.com>
2023-04-15 13:58:33 -0400

    ReBuild code of kill logs....

--- mods/cs_core/core/clua/starters/kill_logs.lua
+++ mods/cs_core/core/clua/starters/kill_logs.lua
@@ -1,42 +1,35 @@
 kills = {
-    terrorist = {
-        exampleuserEULA = {kills = 1, deaths = 0, name = "exampleuserEULA"}
-    },
-    counter = {
-        exampleuserEULA2 = {kills = 50, deaths = 5, name = "exampleuserEULA2"}
-    },
+    terrorist = {},
+    counter = {},
     spectator = {
-        unit = function()
-            error("spectators cant have kills/deaths!")
-        end
+        unit = function() end
     }
 }
---core.after(1, function()
-	    call.register_on_new_matches(function()
-		core.after(5, function()
-		    for pname in pairs(kills.terrorist) do
-		        kills.terrorist[pname] = {kills = 0, deaths = 0}
-		    end
-		    for pname in pairs(kills.counter) do
-		        kills.counter[pname] = {kills = 0, deaths = 0}
-		    end
-		end)
-	    end)
-    
-    call.register_on_player_join_team(function(pname, team)
-        if pname and team then
-            kills[team][pname] = {kills = 0, deaths = 0}
+
+call.register_on_new_matches(function()
+    core.after(5, function()
+        for pname, stats in pairs(kills.terrorist) do
+            stats.kills = 0
+            stats.deaths = 0
+        end
+        for pname, stats in pairs(kills.counter) do
+            stats.kills = 0
+            stats.deaths = 0
         end
     end)
+end)
 
-    call.register_on_kill_player(function(player, enemy, enemyt, playert)
-        if enemy ~= "reason:drown" and enemy ~= "reason:fall" and enemy ~= "reason:table" then
-		if player and enemy and enemyt and playert then
-			kills[enemyt][enemy].kills = kills[enemyt][enemy].kills + 1
-			kills[playert][player].kills = kills[playert][player].deaths + 1
-		end
-	else
-		kills[playert][player].kills = kills[playert][player].deaths + 1
-	end
-    end)
---end)
+call.register_on_player_join_team(function(pname, team)
+    kills[team][pname] = {kills = 0, deaths = 0}
+end)
+
+call.register_on_kill_player(function(player, enemy, enemyt, playert)
+    if enemy ~= "reason:drown" and enemy ~= "reason:fall" and enemy ~= "reason:table" then
+        if player and enemy and enemyt and playert then
+            kills[enemyt][enemy].kills = kills[enemyt][enemy].kills + 1
+            kills[playert][player].kills = kills[playert][player].deaths + 1
+        end
+    else
+        kills[playert][player].kills = kills[playert][player].deaths + 1
+    end
+end)
