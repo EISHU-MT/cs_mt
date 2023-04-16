647279599e14825504447b129a86b81ba7190966
EISHU92 <donatto555@gmail.com>
2023-04-16 11:23:38 -0400

    Fix hud not dissapearing.

--- mods/cs_core/core/defuser_hooks.lua
+++ mods/cs_core/core/defuser_hooks.lua
@@ -7,13 +7,13 @@ function defuser_hooks(timedd)
 		if ask_for_bomb and ask_for_bomb() then
 			if c4.planted then
 				local statspawn = c4.pos
-				if pos.x < statspawn.x + 2 and pos.x > statspawn.x - 2 and pos.y < statspawn.y + 2 and pos.y > statspawn.y - 2 and pos.z < statspawn.z + 2 and pos.z > statspawn.z - 2 then
+				if pos.x < statspawn.x + 1 and pos.x > statspawn.x - 1 and pos.y < statspawn.y + 1 and pos.y > statspawn.y - 1 and pos.z < statspawn.z + 1 and pos.z > statspawn.z - 1 then
 					for playerr in pairs(csgo.team.counter.players) do
 						local player = clua.player(playerr)
 						local pctrl  = player:get_player_control()
-						if pctrl.dig and (player:get_wielded_item():get_name() == "defuser" or player:get_wielded_item():get_name() == ":" or player:get_wielded_item():get_name() == "") then
+						if pctrl.dig and (player:get_wielded_item():get_name() == "core:defuser" or player:get_wielded_item():get_name() == ":" or player:get_wielded_item():get_name() == "") then
 							if csgo.pot[playerr] ~= "terrorist" then
-								if player:get_wielded_item():get_name() == "defuser" then
+								if player:get_wielded_item():get_name() == "core:defuser" then
 									player:hud_change(defuser_huds[playerr], "text", "Defusing in "..tostring(pnts))
 									pnts = pnts - 1
 									if pnts == 0 or pnts < 1 then
@@ -37,6 +37,17 @@ function defuser_hooks(timedd)
 											c4.remove_bomb2()
 											bank.player_add_value(playerr, 200) 
 									end
+								end
+							end
+							
+						else
+							if player:hud_exists(defuser_huds) then
+								player:hud_change(defuser_huds[playerr], "text", " ")
+								if pnts2 ~= defuser_hooks_wait2 then
+									pnts2 = tonumber(defuser_hooks_wait2)
+								end
+								if pnts ~= defuser_hooks_wait then
+									pnts = tonumber(defuser_hooks_wait)
 								end
 							end
 						end
