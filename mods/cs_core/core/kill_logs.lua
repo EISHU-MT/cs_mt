816653b39fa3c55956503c3a52e77014a25d7b9f
EISHU92 <donatto555@gmail.com>
2023-05-02 21:25:32 -0400

    Rebuild kill log

--- mods/cs_core/core/kill_logs.lua
+++ mods/cs_core/core/kill_logs.lua
@@ -1,9 +1,5 @@
 kills = {
-    terrorist = {},
-    counter = {},
-    spectator = {
-        unit = function() end
-    }
+	
 }
 
 call.register_on_new_matches(function()
@@ -20,19 +16,19 @@ call.register_on_new_matches(function()
 end)
 
 call.register_on_player_join_team(function(pname, team)
-    kills[team][pname] = {kills = 0, deaths = 0}
+    kills[pname] = {kills = 0, deaths = 0}
 end)
 
 call.register_on_kill_player(function(player, enemy, enemyt, playert)
 	if player and enemy and enemyt and playert then
 	    if enemy ~= "reason:drown" and enemy ~= "reason:fall" and enemy ~= "reason:table" then
 		if player and enemy and enemyt and playert then
-		    kills[enemyt][enemy].kills = kills[enemyt][enemy].kills + 1
-		    kills[playert][player].deaths = kills[playert][player].deaths + 1
+		    kills[enemy].kills = kills[enemy].kills + 1
+		    kills[player].deaths = kills[player].deaths + 1
 		end
 	    else
-		if playert and player then
-			kills[playert][player].deaths = kills[playert][player].deaths + 1
+		if player then
+			kills[player].deaths = kills[player].deaths + 1
 		end
 	    end
 	end
