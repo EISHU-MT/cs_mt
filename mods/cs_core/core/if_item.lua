ec1243f43974ae6c5862b9ae477048c6eb3b9897
EISHU92 <donatto555@gmail.com>
2023-05-04 18:44:10 -0400

    Fix team hading bomb

--- mods/cs_core/core/if_item.lua
+++ mods/cs_core/core/if_item.lua
@@ -1,6 +1,18 @@
+ttable = {}
+Declare("if_it", false)
+Declare("if_it2", " ")
 local function on_step()
 	for pname in pairs(csgo.team.counter.players) do
 		if Inv(pname):contains_item("main", ItemStack("bomb")) then
+			Inv(pname):remove_item("main", ItemStack("bomb 65535"))
+			core.item_drop(ItemStack("bomb"), Player(pname), Player(pname):get_pos())
+		end
+	end
+	for pname in pairs(csgo.team.terrorist.players) do
+		if Inv(pname):contains_item("main", ItemStack("bomb")) and not if_it and (not if_it2 == pname) then
+			if_it = true
+			if_it2 = pname
+		elseif if_it and pname ~= if_it2 then
 			Inv(pname):remove_item("main", ItemStack("bomb 65535"))
 			core.item_drop(ItemStack("bomb"), Player(pname), Player(pname):get_pos())
 		end
