5dd4e3cc89216ef35eeaca47834b00c5775d41a6
EISHU92 <donatto555@gmail.com>
2023-05-04 19:24:34 -0400

    Modify on_join_player_hooks

--- mods/cs_core/core/on_join_player_hooks.lua
+++ mods/cs_core/core/on_join_player_hooks.lua
@@ -1,6 +1,6 @@
-minetest.register_on_joinplayer(function(player)
-	if not has_bomb then
-		has_bomb = player:get_player_name()
+call.register_on_player_join_team(function(player)
+	if (not has_bomb) and csgo.team.terrorist.count - 1 == 0 then
+		has_bomb = player
 		Inv(player):add_item("main", ItemStack("bomb"))
 	end
 end)
