d2953dece016a3fad7217a0a201b96a7778dfb20
EISHU92 <donatto555@gmail.com>
2023-05-05 18:35:06 -0400

    Check before giving bomb to a player

--- mods/cs_core/core/on_join_player_hooks.lua
+++ mods/cs_core/core/on_join_player_hooks.lua
@@ -1,5 +1,5 @@
-call.register_on_player_join_team(function(player)
-	if (not has_bomb) and csgo.team.terrorist.count - 1 == 0 then
+call.register_on_player_join_team(function(player, team)
+	if (not has_bomb) and csgo.team.terrorist.count - 1 == 0 and team == "terrorist" then
 		has_bomb = player
 		Inv(player):add_item("main", ItemStack("bomb"))
 	end
