b98554e934e92830bb45b4831bc13eb4952d8a24
EISHU92 <donatto555@gmail.com>
2023-05-13 15:41:55 -0400

    Simplify the Randomiser

--- mods/cs_core/cs_tools/init.lua
+++ mods/cs_core/cs_tools/init.lua
@@ -34,64 +34,20 @@ function FindItem(player, item)
 end
 function ItemFind(...) FindItem(...) end
 function RandomPos(pos, rad)
-	--local x_sign = math.random() < 0.25 and -0.5 or 0.5
-	--local z_sign = math.random() < 0.25 and -0.5 or 0.5
-	--local x_offset = x_sign + math.random(rad)
-	--local z_offset = z_sign + math.random(rad)
-	--pos.x = pos.x + x_offset
-	--pos.z = pos.z + z_offset
+	local x_sign = math.random() < 0.25 and -0.5 or 0.5
+	local z_sign = math.random() < 0.25 and -0.5 or 0.5
+	local x_offset = x_sign + math.random(rad) - 0.5
+	local z_offset = z_sign + math.random(rad) - 0.5
+	pos.x = pos.x + x_offset
+	pos.z = pos.z + z_offset
 	return pos
 end
 
 function Randomise(conditionn, etable) -- Primary AI (Only had table parser with random and `IF`)
-	condition = {}
-	--conditionS.timedout = 2
-	--condition.selected, condition.value, condition.value1, condition.value2, condition.value3 = unpack(conditionn:split(" "))
-	condition.selected = unpack(conditionn:split(" "))
-	for number, string in pairs(conditionn:split(" ")) do
-	--print(string, number)
-		if string ~= "Select" then
-			if condition.value == nil then
-				if string then
-					condition.value = string
-					--print(condition.value)
-				else
-					condition.value = "UNIT.FALSE"
-				end
-			else
-				condition["value"..number] = string
-			end
-			
-		end
-	end
-	if condition.selected == "Select" and condition.value then
-		if type(condition.value) == "number" then
-			if type(etable) == "table" then
-				if etable[condition.value] then
-					return etable[condition.value]
-				end
-			end
-		elseif condition.value == "random" then
-			if type(etable) == "table" then
-				if condition.value1 ~= nil then
-					if type(condition.value1) == "number" then
-						tmprandom = math.random(condition.value1)
-						if etable[condition.value1] then
-							return etable[condition.value1]
-						end
-					end
-				elseif condition.value1 == nil then
-					number = 0
-					for numberr, _ in pairs(etable) do
-						number = numberr-- + 1
-					end
-					tmprandom = math.random(number)
-					if etable[tmprandom] then
-						return etable[tmprandom]
-					end
-				end
-			end
-		end
+	if type(etable) == "table" then
+		local numb = #etable
+		local selected = math.random(#etable)
+		return etable[selected] or ""
 	end
 end
 
