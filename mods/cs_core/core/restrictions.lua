7d445d435d532d5b5cab1b9574680ba859fba387
EISHU92 <donatto555@gmail.com>
2023-05-15 11:23:50 -0400

    Put in log every warning/error about restrictions.lua (Test)

--- mods/cs_core/core/restrictions.lua
+++ mods/cs_core/core/restrictions.lua
@@ -1,5 +1,6 @@
 minetest.register_on_prejoinplayer(function(name, ip)
 	if tostring(name):find("__") or tostring(name):find("DST") then
+		core.log("error", "Player "..tostring(name).." tried to enter with not-allowed characters: \"__\" or/and \"DST\"")
 		return "Name cannot contain \"__\" or \"DST\" because that are reserved for developers purposes!"
 	end
 end)
