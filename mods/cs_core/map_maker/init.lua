678d28fe713d0db440f0107623a702f9c1017f75
EISHU92 <donatto555@gmail.com>
2023-05-28 22:18:10 -0400

    Replaced map engine

--- mods/cs_core/map_maker/init.lua
+++
@@ -1,9 +0,0 @@
-local maker = minetest.settings:get_bool("cs_map.mapmaking", false)
-
-if maker then
-	map_maker = {}
-	map_edit = true
-	local modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/"
-	dofile(modpath .. "gui.lua")
-	dofile(modpath .. "map_maker.lua")
-end
