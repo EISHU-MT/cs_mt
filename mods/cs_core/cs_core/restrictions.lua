12329737466363ff48a1391320919ec5c1c71dfc
EISHU92 <donatto555@gmail.com>
2023-06-03 13:09:30 -0400

    Dont make players be able to use "BOT_" in its name

--- mods/cs_core/cs_core/restrictions.lua
+++ mods/cs_core/cs_core/restrictions.lua
@@ -1,6 +1,6 @@
 minetest.register_on_prejoinplayer(function(name, ip)
-	if tostring(name):find("__") or tostring(name):find("DST") then
+	if tostring(name):find("__") or tostring(name):find("DST") or tostring(name):find("BOT_") then
 		core.log("error", "Player "..tostring(name).." tried to enter with not-allowed characters: \"__\" or/and \"DST\"")
-		return "Name cannot contain \"__\" or \"DST\" because that are reserved for developers purposes!"
+		return "Name cannot contain \"__\", \"BOT_\" or \"DST\" because that are used by CS:MT Engine!"
 	end
 end)
