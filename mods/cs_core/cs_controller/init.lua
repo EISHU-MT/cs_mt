cdcf74b255455143c194b70c7bfdb51313aedad2
EISHU92 <donatto555@gmail.com>
2023-06-05 11:47:19 -0400

    Allow to disable/enable CS:MT Engine

--- mods/cs_core/cs_controller/init.lua
+++ mods/cs_core/cs_controller/init.lua
@@ -1,8 +1,4 @@
-cs_core.log("action", "Initialising Database...")
-local modpath = minetest.get_modpath("cs_files")
-mp = {}
-files = {}
-for arg792, directories in pairs(core.get_dir_list(modpath)) do
-files[directories] = modpath .. "/" .. directories -- modpath = /home/user/.minetest/games/csgo/mods/cs-core/cs_files/then /home/user/.minetest/games/csgo/mods/cs-core/cs_files/[directories]
-cs_core.log("action", "Added directory {" .. directories .. "}" )
+local enable = minetest.settings:get_bool("cs_core", true)
+if enable == false then
+	error("Cs:Mt Engine is disabled!")
 end
