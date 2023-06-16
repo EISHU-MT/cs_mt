0767e970aa996ab1519465ae887f63fb2e5def7a
EISHU92 <donatto555@gmail.com>
2023-06-16 17:32:49 -0400

    Repair spectator armor groups

--- mods/cs_core/cs_core/on_join_player_hooks.lua
+++ mods/cs_core/cs_core/on_join_player_hooks.lua
@@ -1,6 +1,16 @@
 call.register_on_player_join_team(function(player, team)
-	if (not has_bomb) and csgo.team.terrorist.count - 1 == 0 and team == "terrorist" then
+	if ((not has_bomb) or (not Player(has_bomb))) and csgo.team.terrorist.count - 1 == 0 and team == "terrorist" then
 		has_bomb = player
 		Inv(player):add_item("main", ItemStack("bomb"))
 	end
 end)
+
+local function hks(dtime)
+	for pname in pairs(csgo.team.spectator.players) do
+		if Player(pname) then
+			Player(pname):set_armor_groups({immortal=1})
+		end
+	end
+end
+
+core.register_globalstep(hks)
