3a523f660d82b2a222f6a4e4ae685903255d220b
EISHU92 <donatto555@gmail.com>
2023-06-27 10:14:40 -0400

    Dont allow players enter with 'null' name

--- mods/cs_core/cs_core/restrictions.lua
+++ mods/cs_core/cs_core/restrictions.lua
@@ -1,5 +1,5 @@
 minetest.register_on_prejoinplayer(function(name, ip)
-	if tostring(name):find("__") or tostring(name):find("DST") or tostring(name):find("BOT_") or tostring(name):find("add_to") then
+	if tostring(name):find("__") or tostring(name):find("DST") or tostring(name):find("BOT_") or tostring(name):find("add_to") or tostring(name):find("null") then
 		core.log("error", "Player "..tostring(name).." tried to enter with not-allowed characters: \"__\" or/and \"DST\"")
 		return "Name cannot contain \"__\", \"BOT_\" or \"DST\" because that are used by CS:MT Engine!"
 	end
