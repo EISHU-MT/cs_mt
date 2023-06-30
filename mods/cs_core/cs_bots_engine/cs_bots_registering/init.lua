5d751cca7dd437ff48f6340934e56d9bcc35b7c3
EISHU92 <donatto555@gmail.com>
2023-06-29 21:36:24 -0400

    Add 1 bot more per team

--- mods/cs_core/cs_bots_engine/cs_bots_registering/init.lua
+++ mods/cs_core/cs_bots_engine/cs_bots_registering/init.lua
@@ -120,3 +120,43 @@ bots.register_bot("cs_bots_registering:schweepes", {
 	wmine = {x = 200, y = 219},
 	sit = {x = 81, y = 160},
 })
+
+bots.register_bot("cs_bots_registering:kriller", {
+	bot_name = "Kriller",
+	rifles = {"rangedweapons:ak47", "rangedweapons:awp"},
+	pistols = {"rangedweapons:beretta", "rangedweapons:deagle"},
+	hp = 20,
+	team = "terrorist",
+	model = "character.b3d",
+	textures = {"red.2.png"},
+	recharge = true,
+	collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
+	sh = 0.6,
+}, { -- Animations
+	stand = {x = 0, y = 79},
+	lay = {x = 162, y = 166},
+	walk = {x = 168, y = 187},
+	mine = {x = 189, y = 198},
+	wmine = {x = 200, y = 219},
+	sit = {x = 81, y = 160},
+})
+
+bots.register_bot("cs_bots_registering:tiago", {
+	bot_name = "Tiago",
+	rifles = {"rangedweapons:ak47", "rangedweapons:awp"},
+	pistols = {"rangedweapons:beretta", "rangedweapons:deagle"},
+	hp = 20,
+	team = "counter",
+	model = "character.b3d",
+	textures = {"blue.png"},
+	recharge = true,
+	collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
+	sh = 0.6,
+}, { -- Animations
+	stand = {x = 0, y = 79},
+	lay = {x = 162, y = 166},
+	walk = {x = 168, y = 187},
+	mine = {x = 189, y = 198},
+	wmine = {x = 200, y = 219},
+	sit = {x = 81, y = 160},
+})
