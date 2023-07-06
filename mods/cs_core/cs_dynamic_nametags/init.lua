b39632214545915a0f3609200bf990efc00129a6
EISHU92 <donatto555@gmail.com>
2023-07-06 19:05:39 -0400

    Add animations, also bomb in pocket is added

--- mods/cs_core/cs_dynamic_nametags/init.lua
+++ mods/cs_core/cs_dynamic_nametags/init.lua
@@ -1,122 +1,159 @@
-ataced = false
+--[[
+	EISHU: "Might have to nuke here when selectable nametags exist >:("
+	Dynamic nametags! JSJSJSJS
+--]]
+-- Example: dnametag.exampleOBJ = {obj = obj, name = "", range = 50, players = {usrdata1, usrdata2}, color = 0x0000, huds = {pname1 = ID}}
+dnametag = {
 
-local def = {
-	initial_properties = {
-			name = "bomb",
-			hp_max = 500,
-			physical = true,
-			collide_with_objects = true,
-			collisionbox = { -0.1, -0.1, -0.1, 0.1, 0.1, 0.1 },
-			selectionbox = { -0.1, -0.1, -0.1, 0.1, 0.1, 0.1, rotate = false },
-			pointable = true,
-			visual = "mesh",
-			visual_size = {x = 1.6, y = 1.6, z = 1.6},
-			mesh = "pocket_bomb.obj",
-			textures = {"pocket_bomb.png"},
-			colors = {},
-			use_texture_alpha = false,
-			--spritediv = {x = 1, y = 1},
-			is_visible = true,
-			makes_footstep_sound = true,
-			--automatic_rotate = 0,
-			automatic_face_movement_dir = false,
-			--automatic_face_movement_max_rotation_per_sec = -1,
-			--backface_culling = false,
-			nametag = "Bomb <Unknown>",
-			static_save = false,
-			damage_texture_modifier = "^[brighten",
-			shaded = true,
-			show_on_minimap = true,
-			dtimer1 = 0,
-			dtimer2 = 0,
-			
-	},
-	on_rightclick = function(self, clicker)
-		local obj = self.object
-		local player = obj:get_attach()
-		if player then
-			if player:is_player() then
-				local name = Name(player)
-				local inv = Inv(player)
-				if inv:contains_item("main", "bomb") then
-					bomb_holder = ""
-					local obj2 = minetest.add_item(obj:get_pos(), ItemStack("bomb"))
-					if obj2 then
-						obj2:set_velocity({ x = math.random(-1, 1), y = 5, z = math.random(-1, 1) })
-						core.chat_send_player(Name(player), core.colorize("#FF4A4A", "You dropped the bomb! By "..Name(clicker)))
-						if type(temporalhud) ~= "table" then
-							temporalhud = {}
-							dropt_bomb_object = obj2
-							for pnamee in pairs(csgo.team.terrorist.players) do
-								if Player(pnamee) then
-									temporalhud[pnamee.." "..FormRandomString(5)] = Player(pnamee):hud_add({
-										hud_elem_type = "waypoint",
-										number = 0xFF6868,
-										name = "Dropped bomb is here! dropt by ".. name,
-										text = "m",
-										world_pos = obj2:get_pos()
-									})
-									hud_events.new(Player(pnamee), {
-										text = ("(!) The bomb is being dropped!"),
-										color = "warning",
-										quick = false,
-									})
+}
+
+dnt_api = {}
+
+dnthud = {
+	hud_elem_type = "waypoint",
+	number = 0x00,
+	name = "",
+	text = "",
+	world_pos = vector.new()
+}
+
+local function is_online(thing)
+	if type(thing) == "userdata" then
+		if thing:is_player() and thing:get_player_name() and thing:get_player_control() then
+			return true
+		else
+			return false
+		end
+	else
+		return Player(thing) ~= nil
+	end
+end
+
+local function on_step(dtime)
+	for i, nt in pairs(dnametag) do
+		if nt.obj:get_yaw() and nt.name and nt.players then
+			for _, player in pairs(nt.players) do
+				if is_online(player) then
+					if nt.range then
+						if vector.distance(nt.obj:get_pos(), player:get_pos()) <= nt.range then
+							if nt.huds[Name(player)] then
+								player:hud_change(nt.huds[Name(player)], "world_pos", nt.obj:get_pos())
+							else
+								local def = dnthud
+								def.world_pos = nt.obj:get_pos()
+								def.name = nt.name
+								def.number = nt.color
+								local id = player:hud_add(def)
+								if id then
+									dnametag[i].huds[Name(player)] = id
 								end
 							end
-							has_bomb = nil
+						else
+							if nt.huds[Name(player)] then
+								player:hud_remove(nt.huds[Name(player)])
+								dnametag[i].huds[Name(player)] = nil
+							end
+						end
+					else
+						if nt.huds[Name(player)] then
+							player:hud_change(nt.huds[Name(player)], "world_pos", nt.obj:get_pos())
+						else
+							local def = dnthud
+							def.world_pos = nt.obj:get_pos()
+							def.name = nt.name
+							def.number = nt.color
+							local id = player:hud_add(def)
+							if id then
+								dnametag[i].huds[Name(player)] = id
+							end
 						end
 					end
+				else
+					dnametag[i].players[_] = nil -- Delete player if not online
 				end
-				self.object:remove()
-			elseif player:get_properties().infotext:find("BOT") then
-				local name = player:get_luaentity():get_bot_name()
-				bomb_holder = ""
-				local obj2 = minetest.add_item(obj:get_pos(), ItemStack("bomb"))
-				if obj2 then
-					obj2:set_velocity({ x = math.random(-1, 1), y = 5, z = math.random(-1, 1) })
-				end
-				if type(temporalhud) ~= "table" then
-					temporalhud = {}
-					dropt_bomb_object = obj
-					for pnamee in pairs(csgo.team.terrorist.players) do
-						if Player(pnamee) then
-							temporalhud[pnamee.." "..FormRandomString(5)] = Player(pnamee):hud_add({
-								hud_elem_type = "waypoint",
-								number = 0xFF6868,
-								name = "Dropped bomb is here! dropt by ".. name,
-								text = "m",
-								world_pos = obj:get_pos()
-							})
-							hud_events.new(Player(pnamee), {
-								text = ("(!) The bomb is being dropped!"),
-								color = "warning",
-								quick = false,
-							})
-						end
-					end
-					has_bomb = nil
-				end
-				self.object:remove()
 			end
 		end
-	end,
-	on_step = function(self)
-		local attached = self.object:get_attach()
-		if attached and (attached:is_player() or bots.is_bot(attached)) then
-			if check_if_bomb() or (attached:get_luaentity() and attached:get_luaentity():get_bot_name() and attached:get_luaentity():get_bot_name() == bomb_holder) then
-				local players = {}
-				for name in pairs(csgo.team.terrorist.players) do
-					table.insert(players, Player(name))
+	end
+end
+
+core.register_globalstep(on_step)
+
+core.register_on_leaveplayer(function(thing)
+	for i, nt in pairs(dnametag) do
+		if nt.players then
+			for _, player in pairs(nt.players) do
+				if Name(player) == Name(thing) then
+					dnametag[i].players[_] = nil
 				end
-				dnt_api.register_nametag("bomb", {name="Bomb", obj=self.object, color=0xFFA84A, players=players, range=40}) -- spam
-				dnt_api.update_hard_players("bomb", players)
-			else
-				dnt_api.remove_dynamic_nametag("bomb")
 			end
+		end
+		if nt.huds[Name(thing)] then
+			dnametag[i].huds[Name(thing)] = nil
+		end
+	end
+end)
+
+function dnt_api.register_nametag(name, def)
+	if name and def.name and def.obj and def.players and def.color then
+		if dnametag[name] == nil then
+			dnametag[name] = {
+				name = def.name,
+				obj = def.obj,
+				color = def.color,
+				players = def.players,
+				range = def.range or nil,
+				huds = {}
+			}
 		else
-			self.object:remove() -- remove if not attached to anything
+			return false
 		end
-	end,
-	animated_bomb_obj = true -- Used when get_luaentity().animated_bomb_obj
-}
-core.register_entity(":c4:bomb_entity", def)
+	else
+		return false
+	end
+end
+
+function dnt_api.insert_player(name, player)
+	if dnametag[name] then
+		table.insert(dnametag[name].players, Player(player))
+	end
+end
+
+function dnt_api.update_hard_players(name, players)
+	if dnametag[name] then
+		dnametag[name].players = players
+	end
+end
+
+function dnt_api.remove_player(name, thing)
+	if dnametag[name] then
+		for i, player in pairs(dnametag[name].players) do
+			if Name(player) == Name(thing) then
+				dnametag[name].players[i] = nil
+			end
+		end
+		for i, pname in pairs(dnametag[name].huds) do
+			if pname == Name(thing) then
+				dnametag[name].huds[i] = nil
+			end
+		end
+	end
+end
+
+function dnt_api.remove_dynamic_nametag(name)
+	if dnametag[name] then
+		local huds = dnametag[name].huds
+		dnametag[name] = nil
+		for i, id in pairs(huds) do
+			for i, player in pairs(core.get_connected_players()) do
+				player:hud_remove(id)
+			end
+		end
+	else
+		return false
+	end
+end
+
+
+
+
+
